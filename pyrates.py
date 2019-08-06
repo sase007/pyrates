c949e2e1d65fd96012caf533cfa6e80d3679b82e
Václav Vidoň <vaclav.vidon@kiwi.com>
2019-08-06 23:14:18 +0200

    added single date functionality

--- pyrates.py
+++ pyrates.py
@@ -2,36 +2,51 @@
 
 import requests, sys, json
 
-def get_rates(base_currency = "", currencies = "", print_debug = False, return_dict = True):
+def get_rates(base_currency = "", currencies = "", single_date = "", date_interval = [""], print_debug = False, return_dict = True):
     
     if isinstance(base_currency, str):
         base_currency = base_currency.upper()
     else:
-        print("Base currency has to be str, " + str(type(base_currency)) + " provided.")
+        print("Unexpected base_currency variable type. Str expected" + str(type(base_currency)) + " given.")
         sys.exit(1)
     
     if isinstance(currencies, list) or isinstance(currencies, str):
         if isinstance(currencies, list) and len(currencies) > 1:
             currencies = [currency.upper() for currency in currencies]
             for currency in currencies:
-                if base_currency in currency:
+                if base_currency in currency and base_currency != "":
                     print("Currencies cannot contain base currency!")
                     sys.exit(1)
         elif isinstance(currencies, list) and len(currencies) == 1:
             currencies = currencies[0].upper()
-            if base_currency in currencies:
+            if base_currency in currencies and base_currency != "":
                 print("Currencies cannot contain base currency!")
                 sys.exit(1)
         elif isinstance(currencies, str):
             currencies = currencies.upper()
-            if base_currency in currencies:
+            if base_currency in currencies and base_currency != "":
                 print("Currencies cannot contain base currency!")
                 sys.exit(1)
     else:
-        print("Unexpected variable type.")
+        print("Unexpected currencies variable type. Str or list expected, " + str(type(currencies)) + "given.")
         sys.exit(1)
-    
+
     url = 'https://api.exchangeratesapi.io/latest?base={}&symbols={}'.format(base_currency, currencies).replace(" ", "").replace("[", "").replace("]", "").replace("'", "")
+
+    if isinstance(single_date, str):
+        if single_date != "":
+            check_single_date = single_date.split('-')
+            
+            if len(check_single_date) != 3:
+                print("Invalid date provided. YYYY-MM-DD format expected, " + single_date + " given.")
+                sys.exit(1)
+            if len(check_single_date[0]) != 4:
+                print("Invalid date provided. YYYY-MM-DD format expected, " + single_date + " given.")
+                sys.exit(1)
+            url = url.replace("latest", single_date)
+    else:
+        print("Unexpected single_date variable type. Str expected, " + str(type(currencies)) + "given.")
+        sys.exit(1)
 
     if print_debug:
         print("\n" + url + "\n")
